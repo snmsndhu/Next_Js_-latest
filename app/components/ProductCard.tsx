73e6e6f40ae1584395435bcf137c3f76d73263cc
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-08 11:14:29 -0500

    client and server components

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import AddToCart from "./AddToCart";
 
 const ProductCard = () => {
   return (
     <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500">
-      Add to cart
+      <AddToCart />
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import ProductCard from "./components/ProductCard";
 export default function Home() {
   return (
     <main>
       <h1>hello world</h1>
       <Link href="/users">Users</Link>
+      <ProductCard />
     </main>
   );
 }
