63cc66ad7fdd17ffd5e6d0a7eb231b81b9392920
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-08 11:06:44 -0500

    css

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ProductCard = () => {
-  return <div></div>;
+  return (
+    <div className="p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500">
+      Add to cart
+    </div>
+  );
 };
 
 export default ProductCard;
