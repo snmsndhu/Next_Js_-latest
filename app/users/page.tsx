d493dad0ea3d73daa93d495c1edb7db0eb23f06f
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-08 11:24:40 -0500

    Data Fetching

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+interface User {
+  id: number;
+  name: string;
+}
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+  return (
+    <div>
+      <>
+        <h1>Users</h1>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </>
+    </div>
+  );
 };
 
 export default UsersPage;
