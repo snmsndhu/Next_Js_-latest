7fb57c17c3dbbf0e9760f47349f61de25507c8c0
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-09 12:38:48 -0500

    layout

---
+++ app/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <div className="flex bg-slate-200">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users"> Users</Link>
+    </div>
+  );
+};
+
+export default NavBar;
