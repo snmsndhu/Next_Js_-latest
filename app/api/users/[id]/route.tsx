41c66d08c6d959767be095d5f7b2a399e083fb99
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-11 15:43:31 -0500

    creating API

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 export function GET(
   request: NextRequest,
@@ -13,8 +14,9 @@ export async function PUT(
   { params }: { params: { id: number } }
 ) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ erro: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
   if (params.id > 10)
     return NextResponse.json({ error: "user not found" }, { status: 400 });
   return NextResponse.json({ id: 1, name: body.name });
