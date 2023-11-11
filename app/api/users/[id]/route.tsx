f8394078dc53f4fc880f357abf02896bd0fc67aa
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-11 15:24:21 -0500

    creating API

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -19,3 +19,12 @@ export async function PUT(
     return NextResponse.json({ error: "user not found" }, { status: 400 });
   return NextResponse.json({ id: 1, name: body.name });
 }
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  return NextResponse.json({});
+}
