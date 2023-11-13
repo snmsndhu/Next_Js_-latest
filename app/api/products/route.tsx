23e2f28affbb87e5604ee5401a11fcbfbea95225
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-13 10:36:40 -0500

    creating API

---
+++ app/api/products/route.tsx
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(request: NextRequest) {
+  return NextResponse.json([
+    {
+      id: 1,
+      name: "Milk",
+      price: 2.5,
+    },
+    {
+      id: 2,
+      name: "Bread",
+      price: 3.5,
+    },
+  ]);
+}
