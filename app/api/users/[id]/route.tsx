010840797efd6ec99d4ca9c43e57a169662ded62
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-14 08:55:01 -0500

    prisma

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -15,15 +15,25 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  if (params.id > 10)
-    return NextResponse.json({ error: "user not found" }, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name });
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user)
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(updatedUser);
 }
 
 export async function DELETE(
