2b1f807a77980cad6ee68dd24e76566bba977539
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-14 09:08:21 -0500

    prisma

--- app/api/products/route.tsx
+++ app/api/products/route.tsx
@@ -1,18 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    {
-      id: 1,
-      name: "Milk",
-      price: 2.5,
-    },
-    {
-      id: 2,
-      name: "Bread",
-      price: 3.5,
-    },
-  ]);
+import { prisma } from "@/prisma/client";
+
+export async function GET(request: NextRequest) {
+  const products = await prisma.product.findMany();
+  return NextResponse.json(products);
 }
 
 export async function POST(request: NextRequest) {
@@ -20,8 +12,11 @@ export async function POST(request: NextRequest) {
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json(
-    { id: 10, name: body.name, price: body.price },
-    { status: 201 }
-  );
+  const newProducts = await prisma.product.create({
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+  return NextResponse.json(newProducts, { status: 201 });
 }
