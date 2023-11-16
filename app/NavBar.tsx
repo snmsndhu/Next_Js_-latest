974c78304605abe82e2beb19fa259174cc36aa7c
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-16 12:41:03 -0500

    next auth

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,11 +3,12 @@ import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className="flex bg-slate-200 p-5">
+    <div className="flex bg-slate-200 p-3 space-x-3">
       <Link href="/" className="mr-5">
         Next.js
       </Link>
       <Link href="/users"> Users</Link>
+      <Link href="/api/auth/signin">Login</Link>
     </div>
   );
 };
