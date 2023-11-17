c31e308a16ee44c5c69bf90248ba83b1505638e2
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-17 12:50:56 -0500

    next auth

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-export default function Home() {
-  getServerSession(authOptions);
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
     <main>
-      <h1>hello world</h1>
+      <h1>hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       <ProductCard />
     </main>
