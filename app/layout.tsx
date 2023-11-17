0bcab867fa835658dcbbdd9c1fbebc47d8ec07ae
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-17 12:35:05 -0500

    next auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import NavBar from "./NavBar";
+import { SessionProvider } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" data-theme="winter">
       <body className={inter.className}>
-        <NavBar />
-        <main className="p-5"> {children}</main>
+        <SessionProvider>
+          <NavBar />
+          <main className="p-5"> {children}</main>
+        </SessionProvider>
       </body>
     </html>
   );
