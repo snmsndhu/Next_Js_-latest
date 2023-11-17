05b1585579c499c84c9b4421e6e3be5f826a105a
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-17 13:04:16 -0500

    next auth

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/new-page", request.url));
-}
-
+import middleware from "next-auth/middleware";
+export default middleware;
 export const config = {
   matcher: ["/users/:id*"],
 };
