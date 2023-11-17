1a1fb5e0d95edd48647016cf8439b238a63dfe52
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-17 12:49:33 -0500

    next auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 export default function Home() {
+  getServerSession(authOptions);
   return (
     <main>
       <h1>hello world</h1>
