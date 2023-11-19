8c4eec438f26dec445efa29ed6f77c39fad195f1
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-18 19:11:40 -0500

    next auth

--- app/register/route.ts
+++ app/register/route.ts
@@ -1,6 +1,7 @@
 import { prisma } from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const schema = z.object({
   email: z.string().email(),
@@ -16,4 +17,18 @@ export async function POST(request: NextRequest) {
   const user = await prisma.user.findUnique({
     where: { email: body.email },
   });
+
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const hashedPassword = await bcrypt.hash(body.password, 10);
+
+  const newUser = await prisma.user.create({
+    data: {
+      email: body.email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json({ email: newUser.email });
 }
