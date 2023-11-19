10af78ac0b6d27d6c557adf0298ea8dbf092a0cb
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-18 20:04:04 -0500

    next auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,16 @@
-import { prisma } from "@/prisma/client";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/prisma/client";
 import bcrypt from "bcrypt";
-
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "Email",
-        },
+        email: { label: "Email", type: "email", placeholder: "Email" },
         password: {
           label: "Password",
           type: "password",
@@ -24,18 +19,14 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials, req) {
         if (!credentials?.email || !credentials.password) return null;
-
         const user = await prisma.user.findUnique({
           where: { email: credentials.email },
         });
-
         if (!user) return null;
-
         const passwordsMatch = await bcrypt.compare(
           credentials.password,
           user.hashedPassword!
         );
-
         return passwordsMatch ? user : null;
       },
     }),
@@ -48,3 +39,7 @@ export const authOptions: NextAuthOptions = {
     strategy: "jwt",
   },
 };
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
