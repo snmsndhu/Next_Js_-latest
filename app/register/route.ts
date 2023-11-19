eee8b854fef0933b1c9f1fc612208d7af709cd5a
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-18 19:02:44 -0500

    next auth

--- app/register/route.ts
+++ app/register/route.ts
@@ -1,10 +1,14 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
-z.object({
+const schema = z.object({
   email: z.string().email(),
   password: z.string().min(5),
 });
 export async function POST(request: NextRequest) {
   const body = await request.json();
+
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
 }
