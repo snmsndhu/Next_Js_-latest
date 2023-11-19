f8ff1696efefb5273c0224606b769fa06d06a72a
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-18 20:13:26 -0500

    sending emails

---
+++ emails/WelcomeTemplate.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  Html,
+  Body,
+  Container,
+  Text,
+  Link,
+  Preview,
+} from "@react-email/components";
+
+const WelcomeTemplate = () => {
+  return <div>WelcomeTemplate</div>;
+};
+
+export default WelcomeTemplate;
