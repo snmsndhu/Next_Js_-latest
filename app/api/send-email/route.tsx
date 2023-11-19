a4529372f3364ee6681f8c82ef3a06385ee2bba6
snmsndhu <107799845+snmsndhu@users.noreply.github.com>
2023-11-18 20:51:27 -0500

    sending emails

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -1,5 +1,15 @@
+import WelcomeTemplate from "@/emails/WelcomeTemplate";
+import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {}
+export async function POST() {
+  await resend.emails.send({
+    from: "...",
+    to: "....",
+    subject: "....",
+    react: <WelcomeTemplate name="..." />,
+  });
+  return NextResponse.json({});
+}
